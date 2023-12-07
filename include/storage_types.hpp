435d782570908f10ee0ba29f4623cfd25c8e918c
Artur <arturbrdej@student.agh.edu.pl>
2023-12-07 21:33:10 +0100

    final

--- include/storage_types.hpp
+++ include/storage_types.hpp
@@ -13,32 +13,44 @@ enum PackageQueueType{
 class IPackageStockpile{
 public:
     using const_iterator = std::list<Package>::const_iterator;
-    void push(Package&&);
-    bool empty() const;
-    size_t size() const;
-    const_iterator cbegin() const {return queue.cbegin();}
-    const_iterator begin() {return queue.begin();}
-    const_iterator cend() const {return queue.cend();}
-    const_iterator end()  {return queue.end();}
-    virtual ~IPackageStockpile() = default;
-protected:
-    std::list<Package> queue;
+    virtual void push(Package&&) = 0;
+    virtual bool empty() const = 0;
+    virtual size_t size() const = 0;
+    virtual const_iterator cbegin() const = 0;
+    virtual const_iterator begin() const = 0;
+    virtual const_iterator cend() const = 0;
+    virtual const_iterator end() const = 0;
+    virtual ~IPackageStockpile() {};
 };
 
 class IPackageQueue : public IPackageStockpile {
 public:
-    Package pop();
-    PackageQueueType get_queue_type();
+    virtual Package pop() = 0;
+    virtual PackageQueueType get_queue_type() = 0;
     virtual ~IPackageQueue() = default;
-protected:
-    PackageQueueType queueType;
 };
 
 class PackageQueue: public IPackageQueue {
 public:
-    PackageQueue(PackageQueueType);
-    ~PackageQueue() = default;
+    PackageQueue(PackageQueueType type);
 
+    //IPackageQueue
+    Package pop() override;
+    PackageQueueType get_queue_type() override {return queueType;}
+
+    //IPackageStockpile
+    void push(Package&& package) override {queue.emplace_back(package);}
+    bool empty() const override {return queue.empty();}
+    size_t size() const override {return queue.size();}
+    const_iterator cbegin() const override {return queue.cbegin();}
+    const_iterator begin() const override {return queue.begin();}
+    const_iterator cend() const override {return queue.cend();}
+    const_iterator end() const override {return queue.end();}
+
+    ~PackageQueue() = default;
+private:
+    std::list<Package> queue;
+    PackageQueueType queueType;
 };
 
 #endif //IMPLEMENTATION_STORAGE_TYPES_HPP
--- src/storage_types.cpp
+++ src/storage_types.cpp
@@ -5,15 +5,7 @@ PackageQueue::PackageQueue(PackageQueueType type) {
     queue = std::list<Package>();
 }
 
-PackageQueueType IPackageQueue::get_queue_type() {
-    return queueType;
-}
-
-void IPackageStockpile::push(Package&& pckg) {
-    queue.emplace_back(std::move(pckg));
-}
-
-Package IPackageQueue::pop() {
+Package PackageQueue::pop() {
     if (queueType == LIFO) {
         auto val = queue.back();
         queue.pop_back();
@@ -23,12 +15,4 @@ Package IPackageQueue::pop() {
         queue.pop_front();
         return val;
     }
-}
-
-bool IPackageStockpile::empty() const{
-    return queue.empty();
-}
-
-size_t IPackageStockpile::size() const{
-    return queue.size();
 }
