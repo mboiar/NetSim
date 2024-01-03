86ead1e8b9c498d0ae209719099bab2453fdc707
Maks Boiar <84125090+mboiar@users.noreply.github.com>
2024-01-03 14:31:25 +0100

    Report notifiers
    add report notifiers

--- include/reports.hpp
+++ include/reports.hpp
@@ -2,9 +2,32 @@
 #define IMPLEMENTATION_REPORTS_HPP
 #include "factory.hpp"
 
-class SpecificTurnsReportNotifier{};
+#include <set>
 
-class IntervalReportNotifier{};
+#include "types.hpp"
+
+/**
+ * Notifies about reports at specific turns.
+ */
+class SpecificTurnsReportNotifier{
+public:
+    SpecificTurnsReportNotifier(std::set<Time> turns): turns_(turns){}
+    bool should_generate_report(Time t) { return turns_.find(t) != turns_.end(); };
+private:
+    std::set<Time> turns_;
+};
+
+
+/**
+ * Notifies about reports every n turns.
+ */
+class IntervalReportNotifier{
+public:
+    IntervalReportNotifier(TimeOffset to): to_(to){}
+    bool should_generate_report(Time t) { return t % to_ == 0; };
+private:
+    TimeOffset to_;
+};
 
 void generate_structure_report(const Factory& factory, std::ostream& os);
 
